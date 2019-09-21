2d4edf4e073b907f506565a89228030e89051d2a
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-09-20 20:24:32 -0700

    commit

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,5 +1,5 @@
-//Function to get current year
+
 document.getElementById('Year').innerHTML = new Date().getFullYear;
 
-//Get Document last modified date
+
 document.getElementById('lastUpdated').innerHTML = " Last Updated: " + document.lastModified;
