76b52ee925f79a5a6c07455b2c924816dbffaa2a
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-09-20 20:09:21 -0700

    commit

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,12 +1,3 @@
-var option = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'number',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-}
+document.getElementById('Year').innerHTML = new Date().getFullYear;
 
-document.getElementById('currentDate').innerHTML = new Date().toLocaleString('en-US', options);
-document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
