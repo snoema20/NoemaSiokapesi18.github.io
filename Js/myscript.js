da1eb7f30abb25cace22cc0065b5bac5f0466d80
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-09-21 01:19:18 -0700

    commit

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,6 +1,11 @@
-//Function to get current year
-document.getElementById('currentYear').innerHTML = new Date().getFullYear();
-
-//Get document last modified date
-document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
-
+var options = {
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long', 
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+}
+document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
+document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
