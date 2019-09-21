96638f7f9549e91934ea1a2b81b54440e7d19ce3
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-09-21 00:43:18 -0700

    commit

--- Js/myscript.js
+++ Js/myscript.js
@@ -3,3 +3,4 @@ document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 
 //Get document last modified date
 document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
+
