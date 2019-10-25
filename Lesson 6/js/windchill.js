4c18285dba189689ab47d2b74612fb869604235e
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-10-25 10:52:26 -0700

    commit

--- Lesson 6/js/windchill.js
+++ Lesson 6/js/windchill.js
@@ -1,11 +1,19 @@
+var tempF = parseFloat(document.getElementsByClassName("temp")[0].textContent);
+var speed = parseFloat(document.getElementsByClassName("windSpeed")[0].textContent);
+var f = calcWindChill(tempF, speed);
+	 document.getElementsByClassName("wChill")[0].textContent - f;
 
-var ws = parseFloat(document.getElementById("wind").innerHTML);     
-var th =  parseInt(document.getElementById("temph").innerHTML);   
-var tl =  parseInt(document.getElementById("templ").innerHTML);  
+function calcWindChill(tempF, speed) {
+	var t = tempF;
+    var s = speed;
+   
+    if (t >=50 && s <= 3) { 
+        return 0;
+    }
+    
 
-
-var t  =  (th + tl) / 2;
-var s = Math.pow(ws, 0.16); 
-var f = 35.74 + (0.6215 * t - 35.75 * s) + (0.4275 * t * s); 
-
-document.getElementById("windChill").innerHTML = Math.round(f * 10) / 10 + '°F';
+	else {
+        var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16));
+        return f;
+    }
+}
