56ee029205094d57ae130935c6df18e52149f59f
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-10-25 10:42:04 -0700

    commit

--- Lesson 6/js/webfont.js
+++ Lesson 6/js/webfont.js
@@ -1,17 +1,12 @@
-
-WebFontConfig = {
-  custom: {
+//WebFont Js
+WebFont.load({
+  google: {
     families: [
-      'Nocturno Display Medium 3',
-      'Nocturno Book 2',
-      'Nocturno Regular Italic 24',
-      'Nocturno Regular 26',
-      'Nocturno Regular 25'
-    ],
-    urls: [
-      'https://fonts.typonine.com/WF-000000-001254.css',
-      'https://fonts.typonine.com/WF-000000-001255.css'
+       'Helvetica',
+       'Trebuchet MS',
+       'Verdana',
+       'sans-serif'
     ]
-  },
-  timeout: 2000
-};
+  }
+});
+
