175ceb723d136a8e7682479876a3d57247bc6e83
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-10-25 00:43:31 -0700

    commit

--- Lesson 6/js/webfont.js
+++ Lesson 6/js/webfont.js
@@ -9,3 +9,20 @@ WebFont.load({
       ]
     }
 });
+
+WebFontConfig = {
+  custom: {
+    families: [
+      'Nocturno Display Medium 3',
+      'Nocturno Book 2',
+      'Nocturno Regular Italic 24',
+      'Nocturno Regular 26',
+      'Nocturno Regular 25'
+    ],
+    urls: [
+      'https://fonts.typonine.com/WF-000000-001254.css',
+      'https://fonts.typonine.com/WF-000000-001255.css'
+    ]
+  },
+  timeout: 2000
+};
