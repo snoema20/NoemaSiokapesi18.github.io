acaf215802b9e76a08e90a1209547dc474c5af52
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-10-28 12:43:44 -0700

    commit

--- Lesson 6/js/webfont.js
+++ Lesson 6/js/webfont.js
@@ -1,12 +1,5 @@
-//WebFont Js
-WebFont.load({
-  google: {
-    families: [
-       'Helvetica',
-       'Trebuchet MS',
-       'Verdana',
-       'sans-serif'
-    ]
-  }
-});
-
+(function(d) {
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+  s.parentNode.insertBefore(wf, s);
+})(document);
