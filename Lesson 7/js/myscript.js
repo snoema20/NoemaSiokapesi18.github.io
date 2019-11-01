3d0575bbec7fe031268151c168b0513914feccbf
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-11-01 02:27:45 -0700

    commit

--- Lesson 7/js/myscript.js
+++ Lesson 7/js/myscript.js
@@ -1,11 +1,103 @@
-var options = {
-    weekday: 'long', 
-    day: 'numeric', 
-    month: 'long', 
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
+//Toggle Menu
+function toggleMenu() { 
+    document.getElementsByClassName("main-nav")[0].classList.toggle("responsive");  
 }
-document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
-document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
+
+//Date function
+function getFullDate() {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dt = new Date();
+    const date = dt.getDate();
+    const monthName = monthNames[dt.getMonth()];
+    const dayName = dayNames[dt.getDay()];
+    const fullYear = dt.getFullYear();
+    const date_value = `${dayName}, ${date} ${monthName} ${fullYear}`;
+    return {
+        fullDate: date_value,
+        weekDayName: dayName,
+        currentYear: fullYear
+    };
+}
+
+function changeSize() {
+    //Call date function
+    var dateInfo = getFullDate();
+    
+    //Initialize elements
+    var friday_banner = document.getElementById('friday-banner');
+    var main_header = document.getElementById('main-header');
+    var today_Date = document.getElementById('todayDate');
+    var current_year = document.getElementById('current-year');
+
+    //Initialize device width
+    var device_width = window.innerWidth;
+
+    //Friday Banner
+    if (dateInfo.weekDayName == 'Friday') {
+        friday_banner.style.display = "block";
+        if (device_width < 320) {
+            main_header.style.paddingTop = '95px';
+        } else if (device_width < 768) {
+            main_header.style.paddingTop = '70px';
+        } else if (device_width < 960) {
+            main_header.style.paddingTop = '50px';
+        } else {
+            main_header.style.paddingTop = '40px';
+        }
+    }    
+    //Footer Date
+    today_Date.innerHTML = dateInfo.fullDate;
+
+    //Function to get current year
+    current_year.innerHTML = dateInfo.currentYear;
+}
+
+//Add Event Listeners 
+window.addEventListener('load', changeSize);
+window.addEventListener('resize', changeSize);
+
+//Resize and show banner 
+changeSize();
+
+//Lazy load image using the Intersection Observer API
+const images = document.querySelectorAll('img[data-src]');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+};
+
+const fetchImage = (url) => {
+  console.log(url)
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+}
+
+const loadImage = (image) => {
+  const src = image.dataset.src;
+  fetchImage(src).then(() => {
+    image.src = src;
+  })
+}
+
+const handleIntersection = (entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.intersectionRatio > 0) {
+      //console.log(entry.intersectionRatio);
+      loadImage(entry.target)
+    }
+  })
+}
+
+// The observer for the images on the page
+const observer = new IntersectionObserver(handleIntersection, options);
+
+images.forEach(img => {
+  observer.observe(img);
+})
