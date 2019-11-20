e47ea0d58ad2a40dbffac6f0772891767e70a4e4
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-11-20 15:32:41 -0800

    coomit

---
+++ Lesson 9/js/windchill.js
@@ -0,0 +1,22 @@
+//WindChill calculator
+var WindChill = document.getElementById("wind-chill");
+
+function getWindChill(){
+    var tempF = parseFloat(document.getElementById("temp").innerHTML);
+    var speed = parseFloat(document.getElementById("wind-speed").innerHTML);
+    if(tempF <= 50 && speed > 3)
+        return Math.floor(calculateWindChill(tempF, speed)) + "&#8457;";
+    else 
+        return 'N/A';
+}
+
+function calculateWindChill(tempF, speed){
+    var powerSpeed = Math.pow(speed, 0.16);
+    var mtempF = 0.6215 * tempF;
+    var mpowerSpeed = 35.75 * powerSpeed;
+    var mtempF_mpowerSpeed = 0.4275 * tempF * powerSpeed;
+    var wChillFomula = 35.74 + mtempF - mpowerSpeed + mtempF_mpowerSpeed;
+    return wChillFomula;
+}
+
+WindChill.innerHTML = getWindChill();
