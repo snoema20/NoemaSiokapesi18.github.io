8e8450679d557adec07ca5fedf64416a760d9ae9
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2019-11-27 01:27:58 -0800

    commit

--- Lesson 11/js/fish.js
+++ Lesson 11/js/fish.js
@@ -94,3 +94,36 @@ fetch(requestURL)
     }
   }
 });
+
+
+//WindChill calculator
+var WindChill = document.getElementById("wind-chill");
+
+function getWindChill(){
+    var tempF = parseFloat(document.getElementById("temp").textContent);
+    var speed = parseFloat(document.getElementById("wind-speed").textContent);
+    if(tempF <= 50 && speed > 3)
+        return Math.floor(calculateWindChill(tempF, speed)) + "&#8457;";
+    else 
+        return 'N/A';
+}
+
+function calculateWindChill(tempF, speed){
+    var powerSpeed = Math.pow(speed, 0.16);
+    var mtempF = 0.6215 * tempF;
+    var mpowerSpeed = 35.75 * powerSpeed;
+    var mtempF_mpowerSpeed = 0.4275 * tempF * powerSpeed;
+    var wChillFomula = 35.74 + mtempF - mpowerSpeed + mtempF_mpowerSpeed;
+    return wChillFomula;
+}
+
+// Load wind chill value after all resources are loaded
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") {
+        WindChill.innerHTML = getWindChill();
+    }
+    
+    if (event.target.readyState === "responsive") {
+        WindChill.innerHTML = getWindChill();
+    }
+});
